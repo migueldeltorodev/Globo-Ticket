fdc709974aa291f3aa664da6c4aa27d09709d77f
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-09-27 16:16:44 -0400

    build (eventList): created the service for getting the events list

---
+++ GloboTicket.TicketManagement.ApplicationProject/Features/Events/Queries/GetEventsList/GetEventsListQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList
+{
+    public class GetEventsListQueryHandler: IRequestHandler<GetEventsListQuery, List<EventsListVm>>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+
+        public GetEventsListQueryHandler(IMapper mapper, IAsyncRepository<Event> eventRepository)
+        {
+            _mapper = mapper;
+            _eventRepository = eventRepository;
+        }
+
+        public async Task<List<EventsListVm>> Handle(GetEventsListQuery request, CancellationToken cancellationToken)
+        {
+            var allEvents = (await _eventRepository.ListAllAsync()).OrderBy(x => x.Date);
+            return _mapper.Map<List<EventsListVm>>(allEvents);
+        }
+    }
+}
