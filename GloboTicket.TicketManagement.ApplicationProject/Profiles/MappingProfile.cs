8818b5f791cb4211ebbb9d67511c566afecbbca7
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-09-27 16:26:14 -0400

    build(MappingProfile): updated the mapping profile for the events and category

---
+++ GloboTicket.TicketManagement.ApplicationProject/Profiles/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Event, EventsListVm>().ReverseMap();
+            CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+        }
+    }
+}
