57ea28632c12e47b7450104a1593217159c8dcaf
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-09-27 16:25:32 -0400

    build(eventDetail): created the methods for getting the event details

---
+++ GloboTicket.TicketManagement.ApplicationProject/Features/Events/Queries/GetEventDetail/GetEventDetailQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail
+{
+    public class GetEventDetailQueryHandler : IRequestHandler<GetEventDetailQuery, EventDetailVm>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IAsyncRepository<Category> _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public GetEventDetailQueryHandler(IAsyncRepository<Event> eventRepository, IAsyncRepository<Category> categoryRepository, IMapper mapper)
+        {
+            _eventRepository = eventRepository;
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<EventDetailVm> Handle(GetEventDetailQuery request, CancellationToken cancellationToken)
+        {
+            var eventEntity = await _eventRepository.GetByIdAsync(request.Id);
+            var eventDetailVm = _mapper.Map<EventDetailVm>(eventEntity);
+
+            var categoryEntity = await _categoryRepository.GetByIdAsync(eventEntity.CategoryId);
+            eventDetailVm.Category = _mapper.Map<CategoryDto>(categoryEntity);
+
+            return eventDetailVm;
+        }
+    }
+}
