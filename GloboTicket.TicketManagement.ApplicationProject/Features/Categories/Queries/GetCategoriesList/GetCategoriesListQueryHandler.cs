f50cfb0e675a7cbc720279be27f1da4b6b8ed6f9
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-09-27 17:28:10 -0400

    build(GetCategoriesList): created a method for getting all categories

---
+++ GloboTicket.TicketManagement.ApplicationProject/Features/Categories/Queries/GetCategoriesList/GetCategoriesListQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList
+{
+    public class GetCategoriesListQueryHandler : IRequestHandler<GetCategoriesListQuery, List<CategoryListVm>>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public GetCategoriesListQueryHandler(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;   
+        }
+        public async Task<List<CategoryListVm>> Handle(GetCategoriesListQuery request, CancellationToken cancellationToken)
+        {
+            var allCategories = (await _categoryRepository.ListAllAsync()).OrderBy(x => x.Name);
+            return _mapper.Map<List<CategoryListVm>>(allCategories);
+        }
+    }
+}
