653142bd8315b8d9668d0554b39930bcaeae37d8
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-09-28 09:14:57 -0400

    feat(DeleteEvent): created a new feature to delete a event

---
+++ GloboTicket.TicketManagement.ApplicationProject/Features/Events/Commands/DeleteEvent/DeleteEventCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Exceptions;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.DeleteEvent
+{
+    public class DeleteEventCommandHandler : IRequestHandler<DeleteEventCommand>
+    {
+        private readonly IEventRepository _eventRepository;
+
+        public DeleteEventCommandHandler(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        public async Task Handle(DeleteEventCommand request, CancellationToken cancellationToken)
+        {
+            var eventEntity = await _eventRepository.GetByIdAsync(request.EventId);
+            if (eventEntity == null) throw new NotFoundException(nameof(Event), request.EventId);
+
+            await _eventRepository.DeleteAsync(eventEntity);
+        }
+    }
+}
